411478d078ca21d49d588b81455ae64c074c839c
AntonioP2004 <116681343+AntonioP2004@users.noreply.github.com>
2023-03-15 14:16:52 -0600

    Update Ex24_5.java
    updated to new version

--- Ex24_5.java
+++ Ex24_5.java
@@ -1,41 +1,33 @@
-package Exercises24_1_3_5;
-
 import java.util.LinkedList;
 
-public class Ex24_5<E> {
-	 @SuppressWarnings("rawtypes")
-	public static LinkedList list  = new LinkedList();
-	  
-	  public static void main(String[] args) {
-		  
-		  System.out.println("List before enqueue: " + list);
-		  System.out.print("List after enqueue: ");
-		  enqueue(list);
-		  System.out.println(list);
-		  
-		  System.out.println(" ");
-		  
-		  System.out.println("List before dequeue: " + list);
-		  System.out.print("List after dequeue: ");
-		  dequeue(list);
-		  System.out.println(list);
-	  }
-	  
-	    @SuppressWarnings("unchecked")
-		public static void enqueue(@SuppressWarnings("rawtypes") LinkedList list) {
-	      list.addLast(list);
-	    }
-	  
-	    public static Object dequeue(@SuppressWarnings("rawtypes") LinkedList list) {
-	      return list.removeFirst();
-	    }
-	  
-	    public int getSize() {
-	      return list.size();
-	    }
-	  
-	    @Override
-	    public String toString() {
-	      return "Queue: " + list.toString();
-	    }
-  }
+@SuppressWarnings({ "rawtypes", "serial" })
+public class Ex24_5<E> extends LinkedList {
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		 
+		System.out.println("List before enqueue: " + list);
+		System.out.print("List after enqueue: ");
+		enqueue(list);
+		System.out.println(list);
+		
+		System.out.println(" ");
+		
+		System.out.println("List before dequeue: " + list);
+		System.out.print("List after dequeue: ");
+		dequeue(list);
+		System.out.println(list);
+	 }
+	
+	 @SuppressWarnings("unchecked")
+	public static void enqueue(LinkedList list) {
+      list.push("Test");
+    }
+
+	public static Object dequeue(LinkedList list) {
+      return list.removeFirst();
+    }
+    
+    public LinkedList getString(LinkedList list) {
+    	return list;
+    }
+}
