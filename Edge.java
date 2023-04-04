69eb2a742261947ffaf63a67be4dc9fffd881620
AntonioP2004 <116681343+AntonioP2004@users.noreply.github.com>
2023-04-04 13:21:24 -0600

    Add files via upload

---
+++ Edge.java
@@ -0,0 +1,15 @@
+package Exercises28_3_5_7;
+
+public class Edge {
+	int u;
+	int v;
+	
+	public Edge(int u, int v) {
+		this.u = u;
+		this.v = v;
+	}
+	
+	public boolean equals(Object o) {
+		return (u == ((Edge)o).u && v == ((Edge)o).v);
+	}
+}
